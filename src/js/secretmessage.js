6edb1942c8aa62c5e70927607afa44845ef8675e
Neel Alex <alexneel@gmail.com>
2021-03-02 17:48:07 -0800

    Secret message technology

---
+++ src/js/secretmessage.js
@@ -0,0 +1,291 @@
+function WordShuffler(holder,opt){
+  var that = this;
+  var time = 0;
+  this.now;
+  this.then = Date.now();
+  
+  this.delta;
+  this.currentTimeOffset = 0;
+  
+  this.word = null;
+  this.currentWord = null;
+  this.currentCharacter = 0;
+  this.currentWordLength = 0;
+
+
+  var options = {
+    fps : 20,
+    timeOffset : 5,
+    textColor : '#000',
+    fontSize : "50px",
+    useCanvas : false,
+    mixCapital : false,
+    mixSpecialCharacters : false,
+    needUpdate : true,
+    colors : [
+      '#afafaf','#010101','#202020',
+      '#676767','#3f3f3f','#434343',
+      '#818181','#dfdfdf','#f0f0f0',
+      '#b3b3b3','#717171','#555555',
+      '#a0a0a0','#959595','#444444',
+      '#888888','#c7c7c7','#9e9e9e',
+      '#bababa'
+    ]
+  }
+
+  if(typeof opt != "undefined"){
+    for(key in opt){
+      options[key] = opt[key];
+    }
+  }
+
+
+  
+  this.needUpdate = true;
+  this.fps = options.fps;
+  this.interval = 1000/this.fps;
+  this.timeOffset = options.timeOffset;
+  this.textColor = options.textColor;
+  this.fontSize = options.fontSize;
+  this.mixCapital = options.mixCapital;
+  this.mixSpecialCharacters = options.mixSpecialCharacters;
+  this.colors = options.colors;
+
+   this.useCanvas = options.useCanvas;
+  
+  this.chars = [
+    'A','B','C','D',
+    'E','F','G','H',
+    'I','J','K','L',
+    'M','N','O','P',
+    'Q','R','S','T',
+    'U','V','W','X',
+    'Y','Z'
+  ];
+  this.specialCharacters = [
+    '!','§','$','%',
+    '&','/','(',')',
+    '=','?','_','<',
+    '>','^','°','*',
+    '#','-',':',';','~'
+  ]
+
+  if(this.mixSpecialCharacters){
+    this.chars = this.chars.concat(this.specialCharacters);
+  }
+
+  this.getRandomColor = function () {
+    var randNum = Math.floor( Math.random() * this.colors.length );
+    return this.colors[randNum];
+  }
+
+  //if Canvas
+ 
+  this.position = {
+    x : 0,
+    y : 50
+  }
+
+  //if DOM
+  if(typeof holder != "undefined"){
+    this.holder = holder;
+  }
+
+  if(!this.useCanvas && typeof this.holder == "undefined"){
+    console.warn('Holder must be defined in DOM Mode. Use Canvas or define Holder');
+  }
+
+
+  this.getRandCharacter = function(characterToReplace){    
+    if(characterToReplace == " "){
+      return ' ';
+    }
+    var randNum = Math.floor(Math.random() * this.chars.length);
+    var lowChoice =  -.5 + Math.random();
+    var picketCharacter = this.chars[randNum];
+    var choosen = picketCharacter.toLowerCase();
+    if(this.mixCapital){
+      choosen = lowChoice < 0 ? picketCharacter.toLowerCase() : picketCharacter;
+    }
+    return choosen;
+    
+  }
+
+  this.writeWord = function(word){
+    this.word = word;
+    this.currentWord = word.split('');
+    this.currentWordLength = this.currentWord.length;
+
+  }
+
+  this.generateSingleCharacter = function (color,character) {
+    var span = document.createElement('span');
+    span.style.color = color;
+    span.innerHTML = character;
+    return span;
+  }
+
+  this.updateCharacter = function (time) {
+    
+      this.now = Date.now();
+      this.delta = this.now - this.then;
+
+       
+
+      if (this.delta > this.interval) {
+        this.currentTimeOffset++;
+      
+        var word = [];
+
+        if(this.currentTimeOffset === this.timeOffset && this.currentCharacter !== this.currentWordLength){
+          this.currentCharacter++;
+          this.currentTimeOffset = 0;
+        }
+        for(var k=0;k<this.currentCharacter;k++){
+          word.push(this.currentWord[k]);
+        }
+
+        for(var i=0;i<this.currentWordLength - this.currentCharacter;i++){
+          word.push(this.getRandCharacter(this.currentWord[this.currentCharacter+i]));
+        }
+
+
+        if(that.useCanvas){
+          c.clearRect(0,0,stage.x * stage.dpr , stage.y * stage.dpr);
+          c.font = that.fontSize + " sans-serif";
+          var spacing = 0;
+          word.forEach(function (w,index) {
+            if(index > that.currentCharacter){
+              c.fillStyle = that.getRandomColor();
+            }else{
+              c.fillStyle = that.textColor;
+            }
+            c.fillText(w, that.position.x + spacing, that.position.y);
+            spacing += c.measureText(w).width;
+          });
+        }else{
+
+          if(that.currentCharacter === that.currentWordLength){
+            that.needUpdate = false;
+          }
+          this.holder.innerHTML = '';
+          word.forEach(function (w,index) {
+            var color = null
+            if(index > that.currentCharacter){
+              color = that.getRandomColor();
+            }else{
+              color = that.textColor;
+            }
+            that.holder.appendChild(that.generateSingleCharacter(color, w));
+          }); 
+        }
+        this.then = this.now - (this.delta % this.interval);
+      }
+  }
+
+  this.restart = function () {
+    this.currentCharacter = 0;
+    this.needUpdate = true;
+  }
+
+  function update(time) {
+    time++;
+    if(that.needUpdate){
+      that.updateCharacter(time);
+    }
+    requestAnimationFrame(update);
+  }
+
+  this.writeWord(this.holder.innerHTML);
+
+  this.setTimeOffset = function (offset) {
+    this.timeOffset = offset;
+  }
+
+  console.log(this.currentWord);
+  update(time);
+}
+
+
+
+
+var text1 = document.getElementById('text1');
+var text2 = document.getElementById('text2');
+var text3 = document.getElementById('text3');
+var text4 = document.getElementById('text4');
+var text5 = document.getElementById('text5');
+var text6 = document.getElementById('text6');
+
+offsetTime = 32000;
+
+var pt1 = new WordShuffler(text1,{
+  textColor : '#fff',
+  timeOffset : 2
+});
+
+
+setTimeout(() => {
+  var pt2 = new WordShuffler(text2,{
+    textColor : '#fff',
+    timeOffset : 2
+  });
+  $("p#text2").hide();
+  $("p#text2").fadeIn(4000);
+  }, offsetTime
+);
+
+setTimeout(() => {
+  var pt3 = new WordShuffler(text3,{
+    textColor : '#fff',
+    timeOffset : 2
+  });
+  $("p#text3").hide();
+  $("p#text3").fadeIn(4000);
+  }, offsetTime*2
+);
+
+setTimeout(() => {
+  var pt4 = new WordShuffler(text4,{
+    textColor : '#fff',
+    timeOffset : 2
+  });
+  $("p#text4").hide();
+  $("p#text4").fadeIn(4000);
+  }, offsetTime*3
+);
+
+setTimeout(() => {
+  var pt5 = new WordShuffler(text5,{
+    textColor : '#fff',
+    timeOffset : 2
+  });
+  $("p#text5").hide();
+  $("p#text5").fadeIn(4000);
+  }, offsetTime*4
+);
+
+setTimeout(() => {
+  var pt6 = new WordShuffler(text6,{
+    textColor : '#fff',
+    timeOffset : 2
+  });
+  $("p#text6").hide();
+  $("p#text6").fadeIn(4000);
+  }, offsetTime*5
+);
+
+function disableSelection(target){
+
+    if (typeof target.onselectstart!="undefined") //IE route
+        target.onselectstart=function(){return false}
+
+    else if (typeof target.style.MozUserSelect!="undefined") //Firefox route
+        target.style.MozUserSelect="none"
+
+    else //All other route (ie: Opera)
+        target.onmousedown=function(){return false}
+
+    target.style.cursor = "default"
+}
+
+disableSelection(document.body);
